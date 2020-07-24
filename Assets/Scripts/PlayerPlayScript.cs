1b17c9fd358ca7032e0da4cfd4ba82c0c5dbbbe5
rohanm92 <randommenon@gmail.com>
2020-07-24 17:54:19 +0530

    Add pooling for waypoint generator in navMesh mode

--- Assets/Scripts/PlayerPlayScript.cs
+++ Assets/Scripts/PlayerPlayScript.cs
@@ -7,6 +7,12 @@ using UnityEngine.AI;
 
 public class PlayerPlayScript : MonoBehaviour
 {
+    public enum WaypointType
+    {
+        Complete,
+        Incomplete
+    }
+
     public bool thirdPersonPlayer;
 
     public float noPathFOV = 120f;
@@ -15,11 +21,17 @@ public class PlayerPlayScript : MonoBehaviour
     public Transform aimTarget;
     public Transform aimAgent;
 
-    public GameObject inCompletePathPrefab;
     public GameObject completePathPrefab;
+    public GameObject inCompletePathPrefab;
+
+    public Transform unusedCompletePool;
+    public Transform unusedIncompletePool;
+
+    public int waypointPoolSize = 50;
 
     Camera camera;
     NavMeshAgent navMeshAgent;
+    WaypointType lastPath;
 
     List<GameObject> pathObjects = new List<GameObject>();
 
@@ -28,6 +40,14 @@ public class PlayerPlayScript : MonoBehaviour
     {
         camera = FindObjectOfType<Camera>();
         navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        for (int i = 0; i <= waypointPoolSize; i++)
+        {
+            GameObject newPoint = Instantiate(completePathPrefab, unusedCompletePool);
+            newPoint.SetActive(false);
+            newPoint = Instantiate(inCompletePathPrefab, unusedIncompletePool);
+            newPoint.SetActive(false);
+        }
 
         // Set Which kind of control is enabled
         navMeshAgent.enabled = !thirdPersonPlayer;
@@ -49,6 +69,44 @@ public class PlayerPlayScript : MonoBehaviour
             navMeshAgent.Warp(new Vector3(0f, 10f, 0f));
         }
     }
+
+    public GameObject GetWaypoint(WaypointType wType)
+    {
+        GameObject wObject = null;
+
+        switch (wType)
+        {
+            case WaypointType.Complete:
+                wObject = unusedCompletePool.GetChild(UnityEngine.Random.Range(0, unusedCompletePool.childCount - 1)).gameObject;
+                break;
+            case WaypointType.Incomplete:
+                wObject = unusedIncompletePool.GetChild(UnityEngine.Random.Range(0, unusedIncompletePool.childCount - 1)).gameObject;
+                break;
+        }
+
+        wObject.SetActive(true);
+        // Return tree with proper material attached to it
+        return wObject;
+    }
+
+    // Returning Waypoints to pool
+    public void ReturnWaypointToPool(GameObject waypointToStore, WaypointType wType)
+    {
+        waypointToStore.SetActive(false);
+        if (wType == WaypointType.Complete)
+        {
+            waypointToStore.transform.SetParent(unusedCompletePool);
+        }
+        else if (wType == WaypointType.Complete)
+        {
+            waypointToStore.transform.SetParent(unusedIncompletePool);
+        }
+
+        waypointToStore.transform.localScale = Vector3.one;
+        waypointToStore.transform.position = Vector3.zero;
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -103,12 +161,10 @@ public class PlayerPlayScript : MonoBehaviour
 
     private void ClearPathObjects()
     {
-        // Pooling?
         // Reverse iterate for removal
         for (int i = pathObjects.Count - 1; i >= 0; i--)
         {
-            GameObject removedObject = pathObjects[i];
-            GameObject.Destroy(removedObject);
+            ReturnWaypointToPool(pathObjects[i], lastPath);
         }
         pathObjects.Clear();
     }
@@ -130,6 +186,8 @@ public class PlayerPlayScript : MonoBehaviour
             case NavMeshPathStatus.PathComplete:
                 Debug.Log("PATH IS POSSIBLE!!!! :) ");
 
+                lastPath = WaypointType.Complete;
+
                 navMeshAgent.SetDestination(point);
                 if (!aimTarget.gameObject.activeSelf)
                 {
@@ -139,18 +197,21 @@ public class PlayerPlayScript : MonoBehaviour
 
                 foreach (Vector3 pathPoint in path.corners)
                 {
-                    // Pooling?
-                    GameObject pathObject = GameObject.Instantiate(completePathPrefab);
+                    GameObject pathObject = GetWaypoint(lastPath);
+                    pathObject.transform.SetParent(transform.parent);
                     pathObject.transform.position = pathPoint;
                     pathObjects.Add(pathObject);
                 }
                 break;
             case NavMeshPathStatus.PathPartial:
                 Debug.Log("ONLY PARTIAL PATH IS POSSIBLE... :( ");
+
+                lastPath = WaypointType.Incomplete;
+
                 foreach (Vector3 pathPoint in path.corners)
                 {
-                    // Pooling?
-                    GameObject pathObject = GameObject.Instantiate(inCompletePathPrefab);
+                    GameObject pathObject = GetWaypoint(lastPath);
+                    pathObject.transform.SetParent(transform.parent);
                     pathObject.transform.position = pathPoint;
                     pathObjects.Add(pathObject);
                 }
